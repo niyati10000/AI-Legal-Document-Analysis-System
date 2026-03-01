08f1bcb943dd2167959da42fc472cd2877838d2f
Niyati Bansal <niyatibansal626@gmail.com>
2026-03-02 00:58:24 +0530

    Fixed bottom syntax error for deployment

--- app.py
+++ app.py
@@ -1103,6 +1103,5 @@ if __name__ == '__main__':
             os.remove('legal_ai.db')
     
     init_db()
-    if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
